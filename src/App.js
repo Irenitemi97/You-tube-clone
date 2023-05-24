ca636082f196713954b2c40458f16ff1857c0269
Irenitemi97 <adediransulyman@hotmail.com>
2023-05-24 07:34:45 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './Components/Navbar';
+import Feed from './Components/Feed';
+import VideoDetails from './Components/VideoDetails';
+import ChannelDetails from './Components/ChannelDetails';
+import SearchFeed from './Components/SearchFeed';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar/>
+         <Routes>
+          <Route path='/' exact element={<Feed/>}/>
+          <Route path='/video/:id'  element={<VideoDetails/>}/>
+          <Route path='/channel/:id' element={<ChannelDetails/>}/>
+          <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+         </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
